9e72a614a9ad7b992b504d74f2cab78ce68a3cf0
damian.krawczyk.9393@gmail.com <Ziomel19>
2020-03-16 18:53:55 +0100

    fixed user managment

--- src/test/java/task1/soft/api/restcontroller/DepartmentRestControllerTest.java
+++ src/test/java/task1/soft/api/restcontroller/DepartmentRestControllerTest.java
@@ -23,7 +23,6 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-;
 
 
 @RunWith(SpringRunner.class)
